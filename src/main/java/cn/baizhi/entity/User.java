8666b8c77eb24a4a73c9dcb8ee6ce1e428d3cc9e
2101class <13141234762@163.com>
2021-08-24 11:39:14 +0800

    这是我们第三个测试的项目 v2.0

---
+++ src/main/java/cn/baizhi/entity/User.java
@@ -0,0 +1,7 @@
+package cn.baizhi.entity;
+
+public class User {
+     private String id;
+     private Integer age;
+
+}
---
+++ src/test/java/cn/baizhi/test/Test1.java
@@ -0,0 +1,9 @@
+package cn.baizhi.test;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("哈哈");
+    }
+}
