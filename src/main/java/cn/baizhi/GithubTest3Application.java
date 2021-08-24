55b0397505606a4eb927148ef88214623065af4e
2101class <13141234762@163.com>
2021-08-24 11:35:32 +0800

    这是我们第三个测试的项目 v1.0

---
+++ src/main/java/cn/baizhi/GithubTest3Application.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTest3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTest3Application.class, args);
+    }
+
+}
